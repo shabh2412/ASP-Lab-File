d5d8151e8ef59c3110059afd80e092f31ab0b053
Rishabh Panesar <rishabh.sagarika@gmail.com>
2020-11-27 01:21:26 +0530

    Completed Experiment 14

---
+++ ASP Lab File/Experiments/14/Experiment14.aspx
@@ -0,0 +1,7 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Experiment14.aspx.cs" Inherits="ASP_Lab_File.Experiments._14.Experiment14" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+    <h1 class="mt-3 mb-2 text-center">Product Inventory</h1>
+    <hr />
+    <asp:GridView ID="GridView1" CssClass="my-4 table table-striped table-bordered table-hover" runat="server">
+    </asp:GridView>
+</asp:Content>
---
+++ ASP Lab File/Experiments/14/Experiment14.aspx.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ASP_Lab_File.Experiments._14
+{
+    public partial class Experiment14 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string CS = ConfigurationManager.ConnectionStrings["DBConnectionString"].ConnectionString;
+            SqlConnection con = new SqlConnection(CS);
+            SqlCommand cmd = new SqlCommand("select * from [tblProducts]", con);
+            con.Open();
+            SqlDataReader reader = cmd.ExecuteReader();
+            GridView1.DataSource = reader;
+            GridView1.DataBind();
+            con.Close();
+        }
+    }
+}
