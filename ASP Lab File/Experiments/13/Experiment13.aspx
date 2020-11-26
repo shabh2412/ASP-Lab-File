1ba90dc2d58be1d968d7bd1e5babcac7072612fa
Rishabh Panesar <rishabh.sagarika@gmail.com>
2020-11-27 01:21:17 +0530

    Completed Experiment 13

---
+++ ASP Lab File/Experiments/13/Experiment13.aspx
@@ -0,0 +1,29 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Experiment13.aspx.cs" Inherits="ASP_Lab_File.Experiments._13.Experiment13" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+    <div class="mt-5">
+        <div class="form-group">
+            <asp:Label ID="usernameLbl" runat="server" AssociatedControlID="UsernameTxt" Text="Username"></asp:Label>
+            <asp:TextBox ID="UsernameTxt" CssClass="form-control" runat="server"></asp:TextBox>
+        </div>
+        <div class="form-group">
+            <asp:Label ID="PasswordLbl" runat="server" AssociatedControlID="PasswordTxt" Text="Password"></asp:Label>
+            <asp:TextBox ID="PasswordTxt" CssClass="form-control" TextMode="Password" runat="server"></asp:TextBox>
+        </div>
+        <asp:Button ID="BtnSubmit" CssClass="btn btn-primary" runat="server" Text="Submit" OnClick="BtnSubmit_Click" />
+        <br />
+        <asp:GridView ID="GridView1" CssClass="my-5 mx-auto" runat="server" CellPadding="4" ForeColor="#333333" GridLines="None">
+            <AlternatingRowStyle BackColor="White" ForeColor="#284775" />
+            <EditRowStyle BackColor="#999999" />
+            <FooterStyle BackColor="#5D7B9D" Font-Bold="True" ForeColor="White" />
+            <HeaderStyle BackColor="#5D7B9D" Font-Bold="True" ForeColor="White" />
+            <PagerStyle BackColor="#284775" ForeColor="White" HorizontalAlign="Center" />
+            <RowStyle BackColor="#F7F6F3" ForeColor="#333333" />
+            <SelectedRowStyle BackColor="#E2DED6" Font-Bold="True" ForeColor="#333333" />
+            <SortedAscendingCellStyle BackColor="#E9E7E2" />
+            <SortedAscendingHeaderStyle BackColor="#506C8C" />
+            <SortedDescendingCellStyle BackColor="#FFFDF8" />
+            <SortedDescendingHeaderStyle BackColor="#6F8DAE" />
+        </asp:GridView>
+    </div>
+</asp:Content>
---
+++ ASP Lab File/Experiments/13/Experiment13.aspx.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Data.SqlClient;
+using System.Web.UI.WebControls;
+using System.Configuration;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace ASP_Lab_File.Experiments._13
+{
+    public partial class Experiment13 : System.Web.UI.Page
+    {
+        string CS = ConfigurationManager.ConnectionStrings["DBConnectionString"].ConnectionString;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection(CS);
+            SqlCommand cmd = new SqlCommand("Select * from [ExperimentThirteen]", con);
+            con.Open();
+            SqlDataReader reader = cmd.ExecuteReader();
+            GridView1.DataSource = reader;
+            GridView1.DataBind();
+            con.Close();
+        }
+        private byte[] CalculateSHA256(string str)
+        {
+            SHA256 sha256 = SHA256Managed.Create();
+            byte[] hashValue;
+            UTF8Encoding objUtf8 = new UTF8Encoding();
+            hashValue = sha256.ComputeHash(objUtf8.GetBytes(str));
+
+            return hashValue;
+        }
+
+
+        protected void BtnSubmit_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection(CS);
+            SqlCommand cmd = new SqlCommand("Insert into ExperimentThirteen values (@Username, @Password)", con);
+            string pwdHash = Encoding.Default.GetString(CalculateSHA256(PasswordTxt.Text));
+
+            cmd.Parameters.AddWithValue("@Username", UsernameTxt.Text);
+            cmd.Parameters.AddWithValue("@Password", pwdHash);
+            try
+            {
+                con.Open();
+
+                cmd.ExecuteNonQuery();
+            }
+            catch (Exception excpetion)
+            {
+                Console.WriteLine(excpetion.Message);
+            }
+            finally
+            {
+                con.Close();
+
+            }
+        }
+    }
+}
